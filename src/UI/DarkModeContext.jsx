a62a800783f83648534180a91954176f28edf387
txm.hien1102 <txm.hien1102@gmail.com>
2024-12-31 13:51:45 +0700

    add UI button

---
+++ src/UI/DarkModeContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+const DarkModeContext = createContext();
+
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.documentElement.classList.toggle("dark", !darkMode);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => useContext(DarkModeContext);
