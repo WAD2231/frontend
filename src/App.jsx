1d3df5ade55cbfb9a8c66374fd317be46d4c6b88
txm.hien1102 <txm.hien1102@gmail.com>
2024-12-31 13:50:50 +0700

    add app config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
 import AdminLayout from "./layouts/admin/AdminLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { privateRoutes } from "@/routes/routes";
+import { privateRoutes, publicRoutes } from "@/routes/routes";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { DarkModeProvider } from './UI/DarkModeContext';
+
 function App() {
   return (
     <ThemeProvider
@@ -12,26 +16,46 @@ function App() {
       disableTransitionOnChange
     >
       <BrowserRouter>
-        <div className="App">
-          <Routes>
-            {privateRoutes.map((route, index) => {
-              let Layout = AdminLayout;
-              const Page = route.components;
-              return (
-                <Route
-                  key={index}
-                  path={route.path}
-                  element={
-                    <Layout>
-                      <Page></Page>
-                    </Layout>
-                  }
-                >
-                </Route>
-              );
-            })}
-          </Routes>
-        </div>
+        <Routes>
+          {privateRoutes.map((route, index) => {
+            let Layout = AdminLayout;
+            const Page = route.components;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+          <Route
+            path="*"
+            element={
+              <DarkModeProvider>
+                <Header />
+                <div className="App">
+                  <Routes>
+                    {publicRoutes.map((route, index) => {
+                      const Page = route.components;
+                      return (
+                        <Route
+                          key={index}
+                          path={route.path}
+                          element={<Page />}
+                        />
+                      );
+                    })}
+                  </Routes>
+                </div>
+                <Footer />
+              </DarkModeProvider>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
